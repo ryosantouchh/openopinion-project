1e3470dcdcebd83be5268f0d0f31959bcfd16f58
Kchanit <chanitaphon.l@ku.th>
2024-11-16 02:23:31 +0700

    feat: fix company card

--- app/companies/page.tsx
+++ app/companies/page.tsx
@@ -5,7 +5,14 @@ export default function CompaniesPage() {
     return (
         <div>
             CompaniesPage
-            <CompanyCard />
+            <CompanyCard
+                name="Agogo"
+                logoUrl="https://play-lh.googleusercontent.com/EN4vEdLW-Y2CudJ01SiOsa3XOv5MdlO7uOVAmm-FuE6gDmPZZshcQDu-SuEI1RpTG0g"
+                avgRating={4.5}
+                reviewCount={100}
+                salaryCount={100}
+                jobCount={100}
+            />
         </div>
     )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,6 @@ export default function Home() {
       <VerifyBlock />
       <PayBlock />
       <Button>Click me</Button>
-      <CompanyCard />
     </main>
   );
 }
