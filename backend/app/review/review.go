4f22b16c01e26caa382da2101b44682f4aa06ef1
Boom Thatpong Paleekan <thatpong.p@arise.tech>
2024-11-17 06:18:11 +0700

    add-company-logo

--- backend/app/review/review.go
+++ backend/app/review/review.go
@@ -80,6 +80,7 @@ type Interview struct {
 type ByCompany struct {
 	Name           string `bson:"name" json:"name"`
 	DomainName     string `bson:"domain" json:"id"`
+	LogoUrl        string `bson:"logoUrl" json:"logoUrl"`
 	Description    string `bson:"description" json:"description"`
 	StaffRange     string `bson:"staff_range" json:"staff_range"`
 	CountReview    int    `json:"reviewCount" bson:"-"`
